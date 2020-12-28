8ffc6c5c7edd69e91e749e7578b6bdc6d948f88a
legenddaniel <siyuanincanada@gmail.com>
2020-12-27 22:21:01 -0500

    update

--- index.js
+++ index.js
@@ -1,114 +1,86 @@
 const fs = require('fs');
 const XLSX = require('xlsx');
+const sizeOf = require('image-size');
 
 /**
- * 
+ * -------------------------------------------------------------------------------
  *
  * 
  * Config Area starts
  * 
  * 
- * 
+ * -------------------------------------------------------------------------------
  */
 
+// Config that normally you don't need to change.
 const originalName = 'template.xlsx';
 const sheetName = 'Sheet1';
+const imgDir = './all products/';
+const missing = ['product images', 'description images', 'english description', 'chinese description'];
 
-// Upload products without ...
+// Must be the year/month of image being uploaded. Check and change every time.
+const imgUploadMonth = '2020/12';
+
+// Upload products without ... Check and change every time.
 const uploadNoProdImg = 0;
 const uploadNoDescImg = 1;
 const uploadNoEngDesc = 1;
 const uploadNoChnDesc = 1;
 
 /**
- * 
+ * -------------------------------------------------------------------------------
  *
  * 
  * Config Area Ends
  * 
  * 
- * 
+ * -------------------------------------------------------------------------------
  */
-
-// The map of columns between the original and the exported file. {col in exported: col in original}
-const map = {
-    D: 'A', // Product name, diff
-    H: 'J', // Short description, diff
-    O: 'K', // Inventory / Stock
-    Z: 'M', // Regular price
-    AN: 'H', // Reorder# / Barcode
-    AO: 'G', // Brand
-    AP: 'C', // Sub Dept, diff
-    AQ: 'E' // 2nd Sub Dept, diff
-};
 
 /**
- * Reformat the excel to the exportable format (column names & positions)
- * Store the formatted excel in a file / js object
  * 
- * Select rows with empty values of prod_img / desc_img / desc_eng / desc_chn
- * Output each result to separate files with sku# and name
- * Delete (splice) the rows with empty values according to the config (config=1)
  * 
- * Output the final file without empty values
  * 
- * Issues:
- * Commas in the value (solution use db-quote, maybe supported by the package itself)
- * Non-ascii characters
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ * Do not need to change codes below
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  */
 
-const workbook = XLSX.readFile(originalName);
-const worksheet = workbook.Sheets[sheetName];
-
-const numOfRows = worksheet['!ref'].match(/\d+$/)[0];
-
-// Final data for export. Initialized by the values of the header. Do not change the structure.
-const data = {
-    "!ref": 'A1:AQ' + numOfRows,
-    A1: { v: 'ID' },
-    B1: { v: 'Type' },
-    C1: { v: 'SKU' },
-    D1: { v: 'Name' },
-    E1: { v: 'Published' },
-    F1: { v: 'Is featured?' },
-    G1: { v: 'Visibility in catalog' },
-    H1: { v: 'Short description' },
-    I1: { v: 'Description' },
-    J1: { v: 'Date sale price starts' },
-    K1: { v: 'Date sale price ends' },
-    L1: { v: 'Tax status' },
-    M1: { v: 'Tax class' },
-    N1: { v: 'In stock?' },
-    O1: { v: 'Stock' },
-    P1: { v: 'Low stock amount' },
-    Q1: { v: 'Backorders allowed?' },
-    R1: { v: 'Sold individually?' },
-    S1: { v: 'Weight (kg)' },
-    T1: { v: 'Length (cm)' },
-    U1: { v: 'Width (cm)' },
-    V1: { v: 'Height (cm)' },
-    W1: { v: 'Allow customer reviews?' },
-    X1: { v: 'Purchase note' },
-    Y1: { v: 'Sale price' },
-    Z1: { v: 'Regular price' },
-    AA1: { v: 'Categories' },
-    AB1: { v: 'Tags' },
-    AC1: { v: 'Shipping class' },
-    AD1: { v: 'Images' },
-    AE1: { v: 'Download limit' },
-    AF1: { v: 'Download expiry days' },
-    AG1: { v: 'Parent' },
-    AH1: { v: 'Grouped products' },
-    AI1: { v: 'Upsells' },
-    AJ1: { v: 'Cross-sells' },
-    AK1: { v: 'External URL' },
-    AL1: { v: 'Button text' },
-    AM1: { v: 'Position' },
-    AN1: { v: 'Meta: reorder_num' },
-    AO1: { v: 'Meta: brand_catg' },
-    AP1: { v: 'Meta: sub_catg_1' },
-    AQ1: { v: 'Meta: sub_catg_2' },
-    "!margins": {
+/**
+* @desc add {t: 's'} and margins to the data matrix
+* @param {object} data 
+* @return {object}
+*/
+const setCellType = data => {
+    Object.keys(data).forEach(cell => {
+        if (cell !== '!ref' && cell !== '!margin') {
+            data[cell].t = 's';
+        }
+    });
+    data['!margins'] = {
         "left": 0.7,
         "right": 0.7,
         "top": 0.75,
@@ -116,75 +88,267 @@ const data = {
         "header": 0.3,
         "footer": 0.3
     }
+    return data;
 };
 
-// Map the necessary data from the original to the exported
-Object.entries(map).forEach(([exported, original]) => {
-    for (let i = 2; i <= numOfRows; i++) {
-        data[exported + i] = { v: worksheet[original + i].v };
+/**
+ * @desc Set image html for product & descriptio images. This function will loop through the image folder and check the existance of images named as the Reorder#
+ * @param {object} data worksheet
+ * @param {string} type 'prodct' | 'decrpt'
+ * @param {number} row
+ * @return {string} 
+ */
+const setImgHtml = (data, type, row) => {
+    if (type !== 'prodct' && type !== 'decrpt') {
+        throw new Error('Must be "prodct" or "decrpt" as the parameter!');
     }
-});
 
-// Set the fixed values. All rows have the same value
-const fixed = {
-    // A1: { v: 'ID' },
-    B: { v: 'simple' },
-    C: { v: '' },
-    E: { v: '1' },
-    F: { v: '0' },
-    G: { v: 'visible' },
-    // I1: { v: 'Description' },
-    J: { v: '' },
-    K: { v: '' },
-    L: { v: 'taxable' },
-    M: { v: '' },
-    // N1: { v: 'In stock?' },
-    P: { v: '' },
-    Q: { v: '0' },
-    R: { v: '0' },
-    S: { v: '' },
-    T: { v: '' },
-    U: { v: '' },
-    V: { v: '' },
-    W: { v: '1' },
-    X: { v: '' },
-    Y: { v: '' },
-    AA: { v: 'Canadian Warehouse' },
-    AB: { v: '' },
-    AC: { v: '' },
-    // AD1: { v: 'Images' },
-    AE: { v: '' },
-    AF: { v: '' },
-    AG: { v: '' },
-    AH: { v: '' },
-    AI: { v: '' },
-    AJ: { v: '' },
-    AK: { v: '' },
-    AL: { v: '' },
-    AM: { v: '0' }
+    let img = 1;
+    let imgName = `${data[`AN${row}`].v}-${type}_${img}.jpg`;
+    let html = '';
+    while (fs.existsSync(imgDir + imgName)) {
+        const dimensions = sizeOf(imgDir + imgName);
+
+        html += `<img class="alignnone size-full" src="/wp-content/uploads/${imgUploadMonth}/${imgName}" alt="" width="${dimensions.width}" height="${dimensions.height}" style="display: inline-block; margin: 0;" />`;
+
+        img++;
+        imgName = `${data[`AN${row}`]}-${type}_${img}.jpg`;
+    }
+
+    return html;
 };
-Object.entries(fixed).forEach(([col, value]) => {
-    for (let i = 2; i <= numOfRows; i++) {
-        data[col + i] = value;
+
+/**
+ * @desc Safely delete rows
+ * @param {object} ws worksheet
+ * @param {number} row 0-based row index
+ * @return {undefined}
+ */
+const deleteRow = (ws, row) => {
+    const range = XLSX.utils.decode_range(ws["!ref"])
+    for (let R = row; R <= range.e.r; ++R) {
+        for (let C = range.s.c; C <= range.e.c; ++C) {
+            ws[XLSX.utils.encode_cell({ r: R, c: C })] = ws[XLSX.utils.encode_cell({ r: R + 1, c: C })]
+        }
     }
-});
+    range.e.r--
+    ws['!ref'] = XLSX.utils.encode_range(range.s, range.e)
+};
 
-
-
-
-
-
-
-// Add property 't' to each cell (key)
-Object.keys(data).forEach(cell => {
-    if (cell !== '!ref' && cell !== '!margin') {
-        data[cell].t = 's';
+/**
+ * @desc Export rows with missing info to separate excel files
+ * @param {string} type Within 'product images', 'description images', 'english description', 'chinese description'
+ * @param {array} data [[reorder#, name], [reorder#, name], ...]. Contains only rows with missing info
+ * @return {undefined}
+ */
+const exportMissing = (type, data) => {
+    if (!missing.includes(type)) {
+        throw new Error('This type was not in the config!');
     }
-});
 
-XLSX.stream.to_csv(data)
-    .pipe(fs.createWriteStream('test.csv'))
-    .on('error', err => {
-        console.log(err);
+    if (!data instanceof Array) {
+        throw new Error('Data must be a nested array with Reorder# and Name!');
+    }
+
+    // Final data for export. Initialized by the values of the header. Do not change the structure.
+    const dataMissing = {
+        A1: { v: 'REORDER#' },
+        B1: { v: 'Name' }
+    };
+
+    // Set values of dataMissing from the argument
+    data.forEach((entry, index) => {
+        dataMissing['A' + (index + 2)] = { v: entry[0] };
+        dataMissing['B' + (index + 2)] = { v: entry[1] };
     });
-// fs.writeFile('xlsx-worksheet.json', JSON.stringify(worksheet), () => {});
+    dataMissing['!ref'] = 'A1:B' + (data.length + 1);
+
+    // Add property 't' to each cell (key)
+    const exportedData = setCellType(dataMissing);
+
+    // Write the new file
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, exportedData, 'Sheet1');
+    try {
+        XLSX.writeFile(workbook, `./missing/missing ${type}.xlsx`);
+        console.log(`Successfully export 'missing ${type}.xlsx' to path: ./missing/`);
+    } catch (e) {
+        console.log(e);
+    }
+
+};
+
+/**
+ * @desc Main function to export CSVs
+ * @param {string} lang 'english' | 'chinese'
+ * @param {obejct} wb workbook
+ * @return {undefined}
+ */
+const exportCSV = (lang, wb) => {
+
+    if (lang !== 'english' && lang !== 'chinese') {
+        throw new Error('Must be "english" or "chinese" as the parameter!');
+    }
+
+    const worksheet = wb.Sheets[sheetName];
+    const numOfRows = worksheet['!ref'].match(/\d+$/)[0];
+
+    // The map of columns between the original and the exported file. {col in exported: col in original}
+    const map = {
+        D: 'A', // Product name, diff
+        H: 'J', // Short description, diff
+        O: 'K', // Inventory / Stock
+        Z: 'M', // Regular price
+        AN: 'H', // Reorder# / Barcode
+        AO: 'G', // Brand
+        AP: 'C', // Sub Dept, diff
+        AQ: 'E' // 2nd Sub Dept, diff
+    };
+    if (lang === 'chinese') {
+        map.D = 'B';
+        map.AP = 'D';
+        map.AQ = 'F';
+        delete map.H; // Will add this back if our client gives us Chinese description
+    }
+
+    // Final data for export. Initialized by the values of the header. Do not change the structure.
+    const data = {
+        "!ref": 'A1:AQ' + numOfRows,
+        A1: { v: 'ID' },
+        B1: { v: 'Type' },
+        C1: { v: 'SKU' },
+        D1: { v: 'Name' },
+        E1: { v: 'Published' },
+        F1: { v: 'Is featured?' },
+        G1: { v: 'Visibility in catalog' },
+        H1: { v: 'Short description' },
+        I1: { v: 'Description' },
+        J1: { v: 'Date sale price starts' },
+        K1: { v: 'Date sale price ends' },
+        L1: { v: 'Tax status' },
+        M1: { v: 'Tax class' },
+        N1: { v: 'In stock?' },
+        O1: { v: 'Stock' },
+        P1: { v: 'Low stock amount' },
+        Q1: { v: 'Backorders allowed?' },
+        R1: { v: 'Sold individually?' },
+        S1: { v: 'Weight (kg)' },
+        T1: { v: 'Length (cm)' },
+        U1: { v: 'Width (cm)' },
+        V1: { v: 'Height (cm)' },
+        W1: { v: 'Allow customer reviews?' },
+        X1: { v: 'Purchase note' },
+        Y1: { v: 'Sale price' },
+        Z1: { v: 'Regular price' },
+        AA1: { v: 'Categories' },
+        AB1: { v: 'Tags' },
+        AC1: { v: 'Shipping class' },
+        AD1: { v: 'Images' },
+        AE1: { v: 'Download limit' },
+        AF1: { v: 'Download expiry days' },
+        AG1: { v: 'Parent' },
+        AH1: { v: 'Grouped products' },
+        AI1: { v: 'Upsells' },
+        AJ1: { v: 'Cross-sells' },
+        AK1: { v: 'External URL' },
+        AL1: { v: 'Button text' },
+        AM1: { v: 'Position' },
+        AN1: { v: 'Meta: reorder_num' },
+        AO1: { v: 'Meta: brand_catg' },
+        AP1: { v: 'Meta: sub_catg_1' },
+        AQ1: { v: 'Meta: sub_catg_2' }
+    };
+
+    // Map the necessary data from the original to the exported
+    Object.entries(map).forEach(([exported, original]) => {
+        for (let i = 2; i <= numOfRows; i++) {
+            data[exported + i] = { v: worksheet[original + i].v };
+        }
+    });
+
+    // Set the fixed values. All rows have the same value, normally
+    const fixed = {
+        B: { v: 'simple' },
+        C: { v: '' },
+        E: { v: '1' },
+        F: { v: '0' },
+        G: { v: 'visible' },
+        J: { v: '' },
+        K: { v: '' },
+        L: { v: 'taxable' },
+        M: { v: '' },
+        P: { v: '' },
+        Q: { v: '0' },
+        R: { v: '0' },
+        S: { v: '' },
+        T: { v: '' },
+        U: { v: '' },
+        V: { v: '' },
+        W: { v: '1' },
+        X: { v: '' },
+        Y: { v: '' },
+        AA: { v: lang === 'english' ? 'Canadian Warehouse' : '加拿大仓' },
+        AB: { v: '' },
+        AC: { v: '' },
+        AE: { v: '' },
+        AF: { v: '' },
+        AG: { v: '' },
+        AH: { v: '' },
+        AI: { v: '' },
+        AJ: { v: '' },
+        AK: { v: '' },
+        AL: { v: '' },
+        AM: { v: '0' }
+    };
+    Object.entries(fixed).forEach(([col, value]) => {
+        for (let i = 2; i <= numOfRows; i++) {
+            data[col + i] = value;
+        }
+    });
+
+    // Set other changing values
+    for (let i = 2; i <= numOfRows; i++) {
+
+        // Self-increasing id like InnoDB
+        data['A' + i] = { v: i - 1 };
+
+        // Has inventory
+        data['N' + i] = { v: data['O' + i] ? '1' : '0' };
+
+        // Get description images html according to reorder#, if exist
+        data['I' + i] = { v: setImgHtml(data, 'decrpt', i) };
+
+        // Get product images html according to reorder#, if exist
+        data['AD' + i] = { v: setImgHtml(data, 'prodct', i) };
+
+    }
+
+    // Export rows with missing info according to the config
+    for (let type of missing) {
+        const dataMissing = [[], []];
+        // exportMissing(type, dataMissing);
+    }
+
+    // Delete rows here, outside the exportMissing()
+
+
+
+    // Add property 't' to each cell (key)
+    const exportedData = setCellType(data);
+
+    // Write the csv
+    try {
+        XLSX.stream
+            .to_csv(exportedData)
+            .pipe(fs.createWriteStream(`exported - ${lang} edition.csv`));
+        console.log(`Successfully export 'exported - ${lang} edition.csv' to path: ./`);
+    } catch (e) {
+        console.log(err);
+    }
+
+}
+
+// Must use original file for both export, i.e. doing identical tasks twice, since the data structure is matched strictly with the original file.
+const workbook = XLSX.readFile(originalName);
+exportCSV('english', workbook);
+exportCSV('chinese', workbook);
